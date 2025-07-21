ee336609cca048521c7257415c21a45a827bcb02
SambitParida <parida.sambit@gmail.com>
2025-07-21 16:38:28 -0400

    load exchange rate

--- nodeploy/executescripts.sql
+++ nodeploy/executescripts.sql
@@ -78,3 +78,15 @@ select
 from                                                
 @sales_dwh.source.my_internal_stg/sales/source=FR/format=json/
 (file_format => sales_dwh.SOURCE.my_json_format);
+
+select 
+    t.$1::date as exchange_dt,
+    to_decimal(t.$2) as usd2usd,
+    to_decimal(t.$3,12,10) as usd2eu,
+    to_decimal(t.$4,12,10) as usd2can,
+    to_decimal(t.$4,12,10) as usd2uk,
+    to_decimal(t.$4,12,10) as usd2inr,
+    to_decimal(t.$4,12,10) as usd2jp
+from 
+     @sales_dwh.source.my_internal_stg/exchange/exchange-rate.csv
+     (file_format => 'sales_dwh.common.csv_format') t
