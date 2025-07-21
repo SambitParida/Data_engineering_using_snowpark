9cfa974a34ee75e5045247e49a651608313faea0
SambitParida <parida.sambit@gmail.com>
2025-07-21 16:41:30 -0400

    load exchange rate

--- nodeploy/executescripts.sql
+++ nodeploy/executescripts.sql
@@ -1,92 +1,73 @@
 USE ROLE SYSADMIN;
 -- USE DATABASE --
 USE DATABASE SALES_DWH;
-
-use schema source;
-list @my_internal_stg/source=IN;
-
-show file formats;
-
-desc file format csv_format;
-
----Read data from internal stage using file format--
-
-select 
-t.$1::text as order_id, 
-    t.$2::text as customer_name, 
-    t.$3::text as mobile_key,
-    t.$4::number as order_quantity, 
-    t.$5::number as unit_price, 
-    t.$6::number as order_valaue,  
-    t.$7::text as promotion_code , 
-    t.$8::number(10,2)  as final_order_amount,
-    t.$9::number(10,2) as tax_amount,
-    t.$10::date as order_dt,
-    t.$11::text as payment_status,
-    t.$12::text as shipping_status,
-    t.$13::text as payment_method,
-    t.$14::text as payment_provider,
-    t.$15::text as mobile,
-    t.$16::text as shipping_address
-from 
-@my_internal_stg/sales/source=IN/format=csv/
-(file_format =>'sales_dwh.source.csv_format' )t;
-
-
-
--- Internal Stage - Query The Parquet Data File Format
-select 
-  $1:"Order ID"::text as orde_id,
-  $1:"Customer Name"::text as customer_name,
-  $1:"Mobile Model"::text as mobile_key,
-  to_number($1:"Quantity") as quantity,
-  to_number($1:"Price per Unit") as unit_price,
-  to_decimal($1:"Total Price") as total_price,
-  $1:"Promotion Code"::text as promotion_code,
-  $1:"Order Amount"::number(10,2) as order_amount,
-  to_decimal($1:"Tax") as tax,
-  $1:"Order Date"::date as order_dt,
-  $1:"Payment Status"::text as payment_status,
-  $1:"Shipping Status"::text as shipping_status,
-  $1:"Payment Method"::text as payment_method,
-  $1:"Payment Provider"::text as payment_provider,
-  $1:"Phone"::text as phone,
-  $1:"Delivery Address"::text as shipping_address
-from 
-     @my_internal_stg/sales/source=US/format=parquet/
-     (file_format => 'sales_dwh.SOURCE.my_parquet_format');
-
-
--- Internal Stage - Query The JSON Data File Format
-select                                                       
-    $1:"Order ID"::text as orde_id,                   
-    $1:"Customer Name"::text as customer_name,          
-    $1:"Mobile Model"::text as mobile_key,              
-    to_number($1:"Quantity") as quantity,               
-    to_number($1:"Price per Unit") as unit_price,       
-    to_decimal($1:"Total Price") as total_price,        
-    $1:"Promotion Code"::text as promotion_code,        
-    $1:"Order Amount"::number(10,2) as order_amount,    
-    to_decimal($1:"Tax") as tax,                        
-    $1:"Order Date"::date as order_dt,                  
-    $1:"Payment Status"::text as payment_status,        
-    $1:"Shipping Status"::text as shipping_status,      
-    $1:"Payment Method"::text as payment_method,        
-    $1:"Payment Provider"::text as payment_provider,    
-    $1:"Phone"::text as phone,                          
-    $1:"Delivery Address"::text as shipping_address
-from                                                
-@sales_dwh.source.my_internal_stg/sales/source=FR/format=json/
-(file_format => sales_dwh.SOURCE.my_json_format);
-
-select 
-    t.$1::date as exchange_dt,
-    to_decimal(t.$2) as usd2usd,
-    to_decimal(t.$3,12,10) as usd2eu,
-    to_decimal(t.$4,12,10) as usd2can,
-    to_decimal(t.$4,12,10) as usd2uk,
-    to_decimal(t.$4,12,10) as usd2inr,
-    to_decimal(t.$4,12,10) as usd2jp
-from 
-     @sales_dwh.source.my_internal_stg/exchange/exchange-rate.csv
-     (file_format => 'sales_dwh.common.csv_format') t
+USE SCHEMA SOURCE;
+LIST @MY_INTERNAL_STG / SOURCE = IN;
+SHOW FILE FORMATS;
+DESC FILE FORMAT CSV_FORMAT;
+---READ DATA FROM INTERNAL STAGE USING FILE FORMAT--
+SELECT T.$1::TEXT AS ORDER_ID,
+    T.$2::TEXT AS CUSTOMER_NAME,
+    T.$3::TEXT AS MOBILE_KEY,
+    T.$4::NUMBER AS ORDER_QUANTITY,
+    T.$5::NUMBER AS UNIT_PRICE,
+    T.$6::NUMBER AS ORDER_VALAUE,
+    T.$7::TEXT AS PROMOTION_CODE,
+    T.$8::NUMBER(10, 2) AS FINAL_ORDER_AMOUNT,
+    T.$9::NUMBER(10, 2) AS TAX_AMOUNT,
+    T.$10::DATE AS ORDER_DT,
+    T.$11::TEXT AS PAYMENT_STATUS,
+    T.$12::TEXT AS SHIPPING_STATUS,
+    T.$13::TEXT AS PAYMENT_METHOD,
+    T.$14::TEXT AS PAYMENT_PROVIDER,
+    T.$15::TEXT AS MOBILE,
+    T.$16::TEXT AS SHIPPING_ADDRESS
+FROM @MY_INTERNAL_STG / SALES / SOURCE = IN / FORMAT = CSV / (FILE_FORMAT => 'SALES_DWH.SOURCE.CSV_FORMAT') T;
+-- INTERNAL STAGE - QUERY THE PARQUET DATA FILE FORMAT
+SELECT $1 :"ORDER ID"::TEXT AS ORDE_ID,
+    $1 :"CUSTOMER NAME"::TEXT AS CUSTOMER_NAME,
+    $1 :"MOBILE MODEL"::TEXT AS MOBILE_KEY,
+    TO_NUMBER($1 :"QUANTITY") AS QUANTITY,
+    TO_NUMBER($1 :"PRICE PER UNIT") AS UNIT_PRICE,
+    TO_DECIMAL($1 :"TOTAL PRICE") AS TOTAL_PRICE,
+    $1 :"PROMOTION CODE"::TEXT AS PROMOTION_CODE,
+    $1 :"ORDER AMOUNT"::NUMBER(10, 2) AS ORDER_AMOUNT,
+    TO_DECIMAL($1 :"TAX") AS TAX,
+    $1 :"ORDER DATE"::DATE AS ORDER_DT,
+    $1 :"PAYMENT STATUS"::TEXT AS PAYMENT_STATUS,
+    $1 :"SHIPPING STATUS"::TEXT AS SHIPPING_STATUS,
+    $1 :"PAYMENT METHOD"::TEXT AS PAYMENT_METHOD,
+    $1 :"PAYMENT PROVIDER"::TEXT AS PAYMENT_PROVIDER,
+    $1 :"PHONE"::TEXT AS PHONE,
+    $1 :"DELIVERY ADDRESS"::TEXT AS SHIPPING_ADDRESS
+FROM @MY_INTERNAL_STG / SALES / SOURCE = US / FORMAT = PARQUET / (
+        FILE_FORMAT => 'SALES_DWH.SOURCE.MY_PARQUET_FORMAT'
+    );
+-- INTERNAL STAGE - QUERY THE JSON DATA FILE FORMAT
+SELECT $1 :"ORDER ID"::TEXT AS ORDE_ID,
+    $1 :"CUSTOMER NAME"::TEXT AS CUSTOMER_NAME,
+    $1 :"MOBILE MODEL"::TEXT AS MOBILE_KEY,
+    TO_NUMBER($1 :"QUANTITY") AS QUANTITY,
+    TO_NUMBER($1 :"PRICE PER UNIT") AS UNIT_PRICE,
+    TO_DECIMAL($1 :"TOTAL PRICE") AS TOTAL_PRICE,
+    $1 :"PROMOTION CODE"::TEXT AS PROMOTION_CODE,
+    $1 :"ORDER AMOUNT"::NUMBER(10, 2) AS ORDER_AMOUNT,
+    TO_DECIMAL($1 :"TAX") AS TAX,
+    $1 :"ORDER DATE"::DATE AS ORDER_DT,
+    $1 :"PAYMENT STATUS"::TEXT AS PAYMENT_STATUS,
+    $1 :"SHIPPING STATUS"::TEXT AS SHIPPING_STATUS,
+    $1 :"PAYMENT METHOD"::TEXT AS PAYMENT_METHOD,
+    $1 :"PAYMENT PROVIDER"::TEXT AS PAYMENT_PROVIDER,
+    $1 :"PHONE"::TEXT AS PHONE,
+    $1 :"DELIVERY ADDRESS"::TEXT AS SHIPPING_ADDRESS
+FROM @SALES_DWH.SOURCE.MY_INTERNAL_STG / SALES / SOURCE = FR / FORMAT = JSON / (FILE_FORMAT => SALES_DWH.SOURCE.MY_JSON_FORMAT);
+SELECT T.$1::DATE AS EXCHANGE_DT,
+    TO_DECIMAL(T.$2) AS USD2USD,
+    TO_DECIMAL(T.$3, 18, 10) AS USD2EU,
+    TO_DECIMAL(T.$4, 18, 10) AS USD2CAN,
+    TO_DECIMAL(T.$5, 18, 10) AS USD2UK,
+    TO_DECIMAL(T.$6, 18, 10) AS USD2INR,
+    TO_DECIMAL(T.$7, 18, 10) AS USD2JP
+FROM @SALES_DWH.SOURCE.MY_INTERNAL_STG / EXCHANGE - RATE - DATA.CSV (
+        FILE_FORMAT => 'SALES_DWH.COMMON_SALES.CSV_FORMAT'
+    ) T;
