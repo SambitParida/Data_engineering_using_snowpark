61a1c20af25bc1cbe750f15795fa8cfa116a3171
SambitParida <parida.sambit@gmail.com>
2025-07-22 16:16:42 -0400

    Create table and sequence in curated schema

---
+++ deploy/V1.1.14__Curated_Schema_Create_Sequence.sql
@@ -0,0 +1,21 @@
+USE ROLE SYSADMIN;
+-- USE DATABASE --
+USE DATABASE SALES_DWH;
+-- USE SCHEMAS --
+-- Following are for curated schema
+-- -----------------------------------
+use schema curated;
+create or replace sequence in_sales_order_seq 
+  start = 1 
+  increment = 1 
+comment='This is sequence for India sales order table';
+
+create or replace sequence us_sales_order_seq 
+  start = 1 
+  increment = 1 
+  comment='This is sequence for USA sales order table';
+
+create or replace sequence fr_sales_order_seq 
+  start = 1 
+  increment = 1 
+  comment='This is sequence for France sales order table';
