d80f8606322ed675d8a13fabde0a65d42dbd8156
SambitParida <parida.sambit@gmail.com>
2025-07-21 22:44:49 -0400

    load exchange rate

--- deploy/V1.1.8__Common_Schema_Load_Exchange_Table.sql
+++ deploy/V1.1.8__Common_Schema_Load_Exchange_Table.sql
@@ -3,18 +3,27 @@ USE ROLE SYSADMIN;
 USE DATABASE SALES_DWH;
 -- USE SCHEMAS --
 USE SCHEMA COMMON_SALES;
-copy into sales_dwh.common_sales.exchange_rate
-from 
+COPY INTO SALES_DWH.COMMON_SALES.EXCHANGE_RATE 
 (
-select 
-    t.$1::date as exchange_dt,
-    to_decimal(t.$2) as usd2usd,
-    to_decimal(t.$3,12,10) as usd2eu,
-    to_decimal(t.$4,12,10) as usd2can,
-    to_decimal(t.$4,12,10) as usd2uk,
-    to_decimal(t.$4,12,10) as usd2inr,
-    to_decimal(t.$4,12,10) as usd2jp
-from 
-     @sales_dwh.source.my_internal_stg/exchange/exchange-rate.csv
-     (file_format => 'sales_dwh.common_sales.my_csv_format') t
+EXCHANGE_DATE,
+USDTOUSD,
+USDTOEU,
+USDTOCAN,
+USDTOUK,
+USDTOINR,
+USDTOJP    
+)
+FROM  (
+    SELECT 
+        T.$1::DATE AS EXCHANGE_DATE,
+        TO_DECIMAL(T.$2) AS USD2USD,
+        TO_DECIMAL(T.$3, 18, 10) AS USD2EU,
+        TO_DECIMAL(T.$4, 18, 10) AS USD2CAN,
+        TO_DECIMAL(T.$5, 18, 10) AS USD2UK,
+        TO_DECIMAL(T.$6, 18, 10) AS USD2INR,
+        TO_DECIMAL(T.$7, 18, 10) AS USD2JP
+    FROM @SALES_DWH.SOURCE.MY_INTERNAL_STG/exchange-rate-data.csv (
+            FILE_FORMAT => 'SALES_DWH.COMMON_SALES.CSV_FORMAT'
+        ) T
+     ON_ERROR = ABORT_STATEMENT
 );
