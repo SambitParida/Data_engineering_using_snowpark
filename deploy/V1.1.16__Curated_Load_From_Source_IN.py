36037f2b4d7f4ac867b8d1b2a1dc38911de542c3
SambitParida <parida.sambit@gmail.com>
2025-07-24 11:47:44 -0400

    Load data to curated schema

--- deploy/V1.1.16__Curated_Load_From_Source_IN.py
+++ deploy/V1.1.16__Curated_Load_From_Source_IN.py
@@ -1,4 +1,5 @@
-from snowflake.snowpark import Session
+from snowflake.snowpark import Session, DataFrame, Window 
+from snowflake.snowpark.functions import col,lit,row_number,rank, coalesce,ifnull
 from dotenv import load_dotenv
 import os
 import sys
@@ -21,51 +22,73 @@ def get_snowpark_session() -> Session:
         }
         return Session.builder.configs(connection_parameters).create()
 
-def ingest_in_sales(session) -> None:
-     session.sql(" \
-                 copy into sales_dwh.source.in_sales_order from ( \
-    select \
-        sales_dwh.source.in_sales_order_seq.nextval as SALES_ORDER_KEY,\
-        t.$1 as ORDER_ID,\
-        t.$2 as CUSTOMER_NAME,\
-        t.$3 as MOBILE_KEY,\
-        t.$4 as ORDER_QUENTITY,\
-        t.$5 as UNIT_PRICE,\
-        t.$6 as ORDER_VALUE,\
-        t.$7 as PROMOTION_CODE,\
-        t.$8 as FINAL_ORDER_AMOUNT,\
-        t.$9 as tax_amount,\
-        t.$10 as ORDER_DT,\
-        t.$11 as PAYMENT_STATUS,\
-        t.$12 as SHIPPING_STATUS,\
-        t.$13 as payment_method,\
-        t.$14 as PAYMENT_PROVIDER,\
-        t.$15 as MOBILE,\
-        t.$16 as SHIPPING_ADDRESS,\
-        metadata$filename as STG_FILE_NAME,\
-        metadata$file_row_number as STG_ROW_NUMBER,\
-        metadata$file_last_modified as STG_LAST_MODIFIED\
-    from \
-    @sales_dwh.source.MY_INTERNAL_STG/sales/source=IN/format=csv/\
-    (\
-        file_format => 'sales_dwh.common_sales.csv_format'\
-    ) t\
-) on_error = 'ABORT_STATEMENT'" \
-                
-     ).collect()
-
+def filter_dataset(df, column_name, filter_critarion) -> DataFrame:
+     return_df = df.filter(col(column_name) == filter_critarion)
+     return return_df
 
 def main():
     session = get_snowpark_session()
+    sales_df = session.sql("select * from sales_dwh.source.in_sales_order")
+    
+    paid_sales_df = filter_dataset(sales_df,'PAYMENT_STATUS','Paid')
+    shipped_sales_df = filter_dataset(paid_sales_df,'SHIPPING_STATUS','Delivered')
 
-    print ("<india sales order> : Before Copy")
-    session.sql("select count (*) from sales_dwh.source.in_sales_order").show()
+    country_sales_df = shipped_sales_df.with_column('Country',lit('IN')).with_column('Region',lit('APAC'))
 
-    ingest_in_sales(session)
+    forex_df = session.sql("select * from sales_dwh.common_sales.exchange_rate")
+    sales_with_forex_df = country_sales_df.join(forex_df,country_sales_df['order_dt']==forex_df['exchange_date'], join_type = 'outer')
 
-    print ("<india sales order> : After Copy")
 
-    session.sql("select count (*) from sales_dwh.source.in_sales_order").show()
+    # results = sales_with_forex_df.select("USD2INR").distinct()
+
+    # # Extract values
+    # names = [row["USD2INR"] for row in results.collect()]
+    # print(names)
+    
+    unique_orders = sales_with_forex_df.with_column('order_rank', rank()\
+                                                    .over(Window.partitionBy(col('order_dt'))\
+                                                          .order_by(col('stg_last_modified')\
+                                                                    .desc())))\
+                                                    .filter(col('order_rank')==1)\
+                                                    .select(col('SALES_ORDER_KEY').alias('unique_sales_order_key'))
+    
+    final_sales_df = unique_orders.join(sales_with_forex_df, unique_orders['unique_sales_order_key']==sales_with_forex_df['SALES_ORDER_KEY'], join_type ='inner')
+
+    # # # Extract values
+    # # names = [row["USD2INR"] for row in results.collect()]
+    # # print(names)
+
+    final_sales_df = final_sales_df.select(col('SALES_ORDER_KEY'),
+                                           col('ORDER_ID'),
+                                           col('ORDER_DT'),
+                                           col('CUSTOMER_NAME'),
+                                           col('MOBILE_KEY'),
+                                           col('Country'),
+                                           col('Region'),
+                                           col('ORDER_QUANTITY'),
+                                           lit('INR').alias('LOCAL_CURRENCY'),
+                                           col('UNIT_PRICE').alias('LOCAL_UNIT_PRICE'),
+                                           col('PROMOTION_CODE').alias('PROMOTION_CODE'),
+                                           col('FINAL_ORDER_AMOUNT').alias('LOCAL_TOTAL_ORDER_AMT'),
+                                           col('TAX_AMOUNT').alias('local_tax_amt'),
+                                           ifnull(col('USD2INR'),lit(1.0)).alias("Exhchange_Rate"),
+                                           (col('FINAL_ORDER_AMOUNT')/ifnull(col('USD2INR'),lit(1.0))).alias('US_TOTAL_ORDER_AMT'),
+                                           (col('TAX_AMOUNT')/ifnull(col('USD2INR'),lit(1.0))).alias('USD_TAX_AMT'),
+                                           col('payment_status'),
+                                           col('shipping_status'),
+                                           col('payment_method'),
+                                           col('payment_provider'),
+                                           col('mobile').alias('conctact_no'),
+                                           col('shipping_address')
+                                                                            )
+    # results = final_sales_df.select("Exhchange_Rate").distinct()
+    # # Extract values
+    # names = [row["Exhchange_Rate"] for row in results.collect()]
+    # print(names)
+
+    #write dataframe to snowflake table#
+    final_sales_df.write.save_as_table("sales_dwh.curated.in_sales_order",mode="overwrite")                                  
+
 
 if __name__ == '__main__':
     main()
