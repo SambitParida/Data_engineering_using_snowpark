eb347ea06f613c0f511916baa90c15c5b9a3d46f
SambitParida <parida.sambit@gmail.com>
2025-07-24 13:10:45 -0400

    create DB objects

--- deploy/sql/V1.1.19__Consumption_Schema_Create_Sequence.sql
+++ deploy/sql/V1.1.19__Consumption_Schema_Create_Sequence.sql
@@ -5,15 +5,9 @@ USE DATABASE SALES_DWH;
 -- Following are for consumption schema
 -- -----------------------------------
 use schema consumption;
-
 create or replace sequence region_dim_seq start = 1 increment = 1;
-
 create or replace sequence product_dim_seq start = 1 increment = 1;
-
 create or replace sequence promo_code_dim_seq start = 1 increment = 1;
-
 create or replace sequence customer_dim_seq start = 1 increment = 1;
-
 create or replace sequence payment_dim_seq start = 1 increment = 1;
-
 create or replace sequence date_dim_seq start = 1 increment = 1;
---
+++ deploy/sql/V1.1.20__Consumption_Schema_Create_Table.sql
@@ -0,0 +1,77 @@
+USE ROLE SYSADMIN;
+-- USE DATABASE --
+USE DATABASE SALES_DWH;
+-- USE SCHEMAS --
+-- Following are for consumption schema
+-- -----------------------------------
+use schema consumption;
+create or replace  table region_dim(
+    region_id_pk number primary key,
+    Country text, 
+    Region text,
+    isActive text(1)
+);
+create or replace  table product_dim(
+    product_id_pk number primary key,
+    Mobile_key text,
+    Brand text, 
+    Model text,
+    Color text,
+    Memory text,
+    isActive text(1)
+);
+
+create or replace  table promo_code_dim(
+    promo_code_id_pk number primary key,
+    promo_code text,
+    isActive text(1)
+);
+
+create or replace  table customer_dim(
+    customer_id_pk number primary key,
+    customer_name text,
+    CONCTACT_NO text,
+    SHIPPING_ADDRESS text,
+    country text,
+    region text,
+    isActive text(1)
+);
+
+create or replace  table payment_dim(
+    payment_id_pk number primary key,
+    PAYMENT_METHOD text,
+    PAYMENT_PROVIDER text,
+    country text,
+    region text,
+    isActive text(1)
+);
+
+create or replace  table date_dim(
+    date_id_pk int primary key,
+    order_dt date,
+    order_year int,
+    oder_month int,
+    order_quater int,
+    order_day int,
+    order_dayofweek int,
+    order_dayname text,
+    order_dayofmonth int,
+    order_weekday text
+);
+
+create or replace table sales_fact (
+ order_id_pk number(38,0),
+ order_code varchar(),
+ date_id_fk number(38,0),
+ region_id_fk number(38,0),
+ customer_id_fk number(38,0),
+ payment_id_fk number(38,0),
+ product_id_fk number(38,0),
+ promo_code_id_fk number(38,0),
+ order_quantity number(38,0),
+ local_total_order_amt number(10,2),
+ local_tax_amt number(10,2),
+ exhchange_rate number(15,7),
+ us_total_order_amt number(23,8),
+ usd_tax_amt number(23,8)
+);
