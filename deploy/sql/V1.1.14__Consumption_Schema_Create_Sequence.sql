364d26953a92846ce7e8d9818f1e5e8ae239c4c2
SambitParida <parida.sambit@gmail.com>
2025-07-24 13:04:57 -0400

    create sequences in consuption schema

---
+++ deploy/sql/V1.1.14__Consumption_Schema_Create_Sequence.sql
@@ -0,0 +1,19 @@
+USE ROLE SYSADMIN;
+-- USE DATABASE --
+USE DATABASE SALES_DWH;
+-- USE SCHEMAS --
+-- Following are for consumption schema
+-- -----------------------------------
+use schema consumption;
+
+create or replace sequence region_dim_seq start = 1 increment = 1;
+
+create or replace sequence product_dim_seq start = 1 increment = 1;
+
+create or replace sequence promo_code_dim_seq start = 1 increment = 1;
+
+create or replace sequence customer_dim_seq start = 1 increment = 1;
+
+create or replace sequence payment_dim_seq start = 1 increment = 1;
+
+create or replace sequence date_dim_seq start = 1 increment = 1;
